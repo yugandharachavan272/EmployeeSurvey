f543d98b3a6c450517cf43a38fe612efdd803962
yugandharachavan272 <47026857+yugandharachavan272@users.noreply.github.com>
2019-02-13 18:09:00 +0530

    update

--- employee_survey/employee_survey_app/forms.py
+++ employee_survey/employee_survey_app/forms.py
@@ -218,5 +218,5 @@ class ResponseForm(models.ModelForm):
                             a.save()
                     except Exception as e:
                         print(" exception in save ", e)
-                         a.save()
+                        a.save()
         return response
