926921627ebbe460b19e89fbb5fffe0c7ebb5a04
yugandharachavan272 <47026857+yugandharachavan272@users.noreply.github.com>
2019-03-15 16:29:38 +0530

    Updates in Test Cases

--- employee_survey/employee_survey_app/tests.py
+++ employee_survey/employee_survey_app/tests.py
@@ -1,8 +1,8 @@
-from django.test import TestCase
+from django.test import TestCase, TransactionTestCase
 from django.urls import reverse
 from django.contrib.auth.models import Permission, Group
 from .forms import UserForm, ResponseForm
-from .models import User, Organisation, Category, Survey
+from .models import User, Organisation, Category, Survey, Question
 
 
 # Create your tests here.
@@ -18,11 +18,6 @@ class UserFormTest(TestCase):
     def test_UserForm_valid(self):
         form = UserForm(data={'username': "user", 'password': "user", 'email': "user@mp.com"})
         self.assertTrue(form.is_valid())
-
-    # Invalid Form Data
-    def test_UserForm_invalid(self):
-        form = UserForm(data={'username': "", 'password': "mp", 'email': ""})
-        self.assertFalse(form.is_valid())
 
 
 # Model Test Cases
@@ -49,6 +44,21 @@ class CategoryTest(TestCase):
         w = self.create_Category()
         self.assertTrue(isinstance(w, Category))
         self.assertEqual(w.__str__(), w.name)
+
+
+class QuestionTest(TransactionTestCase):
+    def setUp(self):
+        s = Survey.objects.create(name='test survey')
+        if isinstance(s, Survey):
+            w = Category.objects.create(name='', survey_id=s.id)
+            self.survey = s
+            self.category = w
+
+    def test_question_creation(self):
+        w = Question.objects.create(text='test question', required=False, category=self.category,
+                                    survey=self.survey, question_type=1, choices='')
+        self.assertTrue(isinstance(w, Question))
+        self.assertEqual(w.__str__(), w.text)
 
 
 #  View Test Cases
@@ -94,3 +104,61 @@ class SurveyReportTest(TestCase):
             else:
                 self.assertEqual(response.context['user'], survey_item.user.id)
 
+
+class EmployeeSurveyTest(TestCase):
+    def setUp(self):
+        # Create two users
+        test_user = User.objects.create_user(username='test_user', password='1X<ISRUkw+tuK')
+
+        test_user.save()
+
+    def test_redirect_if_not_logged_in(self):
+        response = self.client.get(reverse('employee_survey_app:my_surveys'))
+        self.assertRedirects(response, '/employee_survey_app/user_login/?next=/employee_survey_app/my_surveys/')
+
+    def test_logged_in_uses_correct_template(self):
+        login = self.client.login(username='test_user', password='1X<ISRUkw+tuK')
+        response = self.client.get(reverse('employee_survey_app:my_surveys'))
+
+        # Check our user is logged in
+        self.assertEqual(str(response.context['user']), 'test_user')
+        # Check that we got a response "success"
+        self.assertEqual(response.status_code, 200)
+
+        # Check we used correct template
+        self.assertTemplateUsed(response, 'employee_survey_app/employee_surveys.html')
+
+    def test_assigned_survey_list(self):
+        login = self.client.login(username='test_user', password='1X<ISRUkw+tuK')
+        response = self.client.get(reverse('employee_survey_app:my_surveys'))
+
+        # Check our user is logged in
+        self.assertEqual(str(response.context['user']), 'test_user')
+        # Check that we got a response "success"
+        self.assertEqual(response.status_code, 200)
+
+        # Check that initially have any survey in list
+        self.assertTrue('UsersSurveys' in response.context)
+
+
+class EmployeeSurveyDetail(TestCase):
+    def setUp(self):
+        test_user = User.objects.create_user(username='test_user', password='1X<ISRUkw+tuK')
+        test_user.save()
+        s = Survey.objects.create(name='test survey')
+        if isinstance(s, Survey):
+            c = Category.objects.create(name='', survey_id=s.id)
+            self.survey = s
+            if isinstance(c, Category):
+                self.category = c
+                q = Question.objects.create(text='test question', required=False, category=self.category,
+                                            survey=self.survey, question_type=1, choices='')
+                self.question = q
+
+    # Valid Form Data
+    def test_ResponseForm_valid(self):
+        login = self.client.login(username='test_user', password='1X<ISRUkw+tuK')
+        response = self.client.get(reverse('employee_survey_app:my_surveys/'+str(self.survey.id)+''))
+        form = ResponseForm(survey=self.survey, user=response.context['user'], is_finished=False,
+                            user_response_id=0, initial={'user': response.context['user'], 'comments': ''})
+        self.assertTrue(form.is_valid())
